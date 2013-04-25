cb4d75fe38a8d9e99c88e45d5b5fce3ddaf52b80
Justin Spears <speajus@gmail.com>
2013-04-24 21:52:44 -0400

    adding jstl operator gt,lt,ne,eq,and,or,div,mod,le,ge,emty

--- test/test-el.js
+++ test/test-el.js
@@ -4,6 +4,11 @@ var run = function (expr, obj) {
     return p.run(p.parse(expr), obj||{});
 }
 module.exports = {
+    'fn:callme':function(test){
+        test.equals(run('fn.callme(1)', {fn:{callme:function(){ return 3}}}), 3, 'calling func');
+        test.done();
+    }
+    ,
     'a and b':function(test){
         test.equals(run('1 and 2'), run('1 && 2'), 'Why do you feel?');
         test.done();
@@ -23,6 +28,7 @@ module.exports = {
         test.ok(run('empty test', {test: []}), "empty array test");
         test.ok(!run('empty test', {test: [1]}), "not empty array test");
         test.ok(run('empty test', {test: ''}), "empty string test");
+        test.ok(run('empty test', {test: null}), "empty null test");
         test.ok(!run('empty test', {test: 'y'}), "not empty string test");
         test.done();
     },
@@ -35,6 +41,14 @@ module.exports = {
         test.ok(run('1 ne 2'), '1 ne 2');
         test.ok(!run('1 ne 1'), '1 ne 1');
 
+        test.done();
+    },
+    'a mod b':function(test){
+        test.equals(run('3 mod 6'), 3);
+        test.done();
+    },
+    'a div b':function(test){
+        test.equals(run('6 div 3'), 2);
         test.done();
     }
 }
